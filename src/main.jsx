1fbff3013ef951d06d560edeb2a8968cba361b24
saisreeman1 <sreemansaimadhani@gmail.com>
2026-04-24 12:46:13 +0530

    added vercel analytics and seo logic

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+import { Analytics } from "@vercel/analytics/react" // Import this
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <Analytics /> 
+  </React.StrictMode>
+)
